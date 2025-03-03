cbd138d7e83c3f4f490bbcc4f3419f36eb2c1375
Fakkiie <landonhadre04@gmail.com>
2025-03-03 17:28:13 -0500

    added branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,15 @@
 
 export default function Home() {
   return (
-    <div>hi</div>
+    <div>
+      <div className="flex flex-col items-center h-screen">
+        <h1>Welcome to JSOSIF Internal Platform</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Velit illum optio ipsam perferendis tenetur et facilis blanditiis neque,
+          libero alias perspiciatis illo ad possimus consequatur nostrum atque minima eos nesciunt.</p>
+      </div>
+
+
+    </div>
   );
 }
