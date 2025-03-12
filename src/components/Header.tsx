8ee68e21215f99b71268b0a2d61481359e636b42
Kevin Neave <kneave18@gmail.com>
2025-03-12 19:42:09 -0400

    added styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-// Header component, 
+"use client";
+// Header component,
 
 import React, { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
@@ -6,142 +7,145 @@ import Link from "next/link";
 import Image from "next/image";
 import jsosifbanner from "../assets/jsosifbanner.png";
 
-
-
 export default function Header() {
-    // Ensures header stays at the top during scroll
-    const pathname = usePathname();
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const [menuOpen, setMenuOpen] = useState(false);
-    const handleScroll = () => {
-        const position = window.scrollY;
-        setScrollPosition(position);
+  // Ensures header stays at the top during scroll
+  const pathname = usePathname();
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const handleScroll = () => {
+    const position = window.scrollY;
+    setScrollPosition(position);
+  };
+
+  const paths = [
+    {
+      name: "Home",
+      href: "/home",
+    },
+    {
+      name: "Learning Resources",
+      href: "/learningresources",
+    },
+    {
+      name: "Teams",
+      href: "/teams",
+      childPaths: [
+        {
+          name: "Financial Institutions",
+          href: "/financialinstitutions",
+        },
+        {
+          name: "Tech Media Telecommunications",
+          href: "/telemediatelecommunications",
+        },
+        {
+          name: "Consumer & Retail",
+          href: "/consumer&retail",
+        },
+        {
+          name: "Industrials & Natural Resources",
+          href: "/industrials&retail",
+        },
+        {
+          name: "Health Care",
+          href: "/healthcare",
+        },
+        {
+          name: "Fixed Income & Real Estate",
+          href: "/fixedincome&realestate",
+        },
+      ],
+    },
+  ];
+
+  // Adds event listener to listen for scroll events
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll, { passive: true });
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, []);
 
-    const paths = [
-        {
-            name: "Home",
-            href: "/home",
-        },
-        {
-            name: "Learning Resources",
-            href: "/learningresources",
-        },
-        {
-            name: "Teams",
-            href: "/teams",
-            childPaths: [
-                {
-                    name: "Placeholder1",
-                    href: "/placholder1",
-                },
-                {
-                    name: "Placeholder2",
-                    href: "/placholder2",
-                },
-            ]
-        }
-    ];
-
-    // Adds event listener to listen for scroll events
-    useEffect(() =>{
-        window.addEventListener("scroll", handleScroll, { passive: true});
-
-        return () => {
-            window.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
-    return (
-        <header
-            className={`flex items-center justify-center py-4 bg-white shadow-md z-50 ${
-                scrollPosition > 0 ? "fixed w-full" : "absolute w-full"
-            }`}
+  return (
+    <header
+      className={`flex items-center justify-center py-4 bg-white shadow-md z-50 ${
+        scrollPosition > 0 ? "fixed w-full" : "absolute w-full"
+      }`}
+    >
+      <div
+        className={`fixed h-screen w-screen top-0 left-0 bg-black z-30 transition-all ${
+          !menuOpen
+            ? "bg-opacity-0 pointer-events-none"
+            : "bg-opacity-20 pointer-events-auto"
+        }`}
+      />
+      <div
+        className={`fixed top-0 left-0 z-40 transition-all duration-300 ${
+          menuOpen
+            ? "translate-x-0"
+            : "-translate-x-[90vw] sm:-translate-x-[30rem]"
+        }`}
+      ></div>
+      {/* Logo Section */}
+      <div className="mx-4 md:mx-20 xl:mx-52 w-full, flex items-center xl:justify-between relative">
+        <button
+          onClick={() => setMenuOpen(true)}
+          aira-label="Back to home"
+          className="hover:cursor-pointer mr-4 lg:mr-[8rem] rounded-full p-2 hover:text-[#0E5791] text-gray-600 xl:hidden"
         >
-            <div
-                className = {`fixed h-screen w-screen top-0 left-0 bg-black z-30 transition-all ${
-                    !menuOpen
-                    ? "bg-opacity-0 pointer-events-none"
-                    : "bg-opacity-20 pointer-events-auto"
-                }`}
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 24 24"
+          >
+            <path fill="currentColor" d="M3 4h18v2H3zm0 7h18v2H3z" />
+          </svg>
+        </button>
+        {/* Desktop home button */}
+        <div className="hover:cursor-pointer w-fit mr-12 shrink-0 md:block hidden">
+          <Link
+            href="/"
+            passHref
+            legacyBehavior
+            aira-labal="Back to home"
+            className="hover:cursor-pointer"
+          >
+            <Image
+              src={jsosifbanner}
+              alt="Logo"
+              height={80}
+              width={300}
+              className="h-12 md:h-16 xl:h-20 object-contain hover:cursor-pointer"
             />
-
-            <div
-                className = {`fixed top-0 left-0 z-40 transition-all duration-300 ${
-                    menuOpen
-                        ? "translate-x-0"
-                        : "-translate-x-[90vw] sm:-translate-x-[30rem]"
-                }`}
-            >
-            </div>
-            // Logo Section 
-            <div>
-                <button
-                    onClick={() => setMenuOpen(true)}
-                    aira-label="Back to home"
-                    className="hover:cursor-pointer mr-4 lg:mr-[8rem] rounded-full p-2 hover:text-[#0E5791] text-gray-600 xl:hidden"
+          </Link>
+        </div>
+        {/* Navigation Links */}
+        <nav className="xl:flex space-x-8 text-gray-600 hidden">
+          {paths.map(({ name, href }) => {
+            return (
+              <Link
+                href={href}
+                passHref
+                legacyBehavior
+                key={name}
+                aria-label={`Navigate to ${name}`}
+                className={"hover:cursor-pointer"}
+              >
+                <a
+                  aira-disabled="true"
+                  className={`hover:text-[#0E5791] hover:cursor-pointer ${
+                    pathname === href && "text-primary"
+                  }`}
                 >
-
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="32"
-                        height="32"
-                        viewBox="0 0 24 24"
-                    >
-
-                        <path 
-                            fill="currentColor"
-                            d="M3 4h18v2H3zm0 7h18v2H3z"
-                        />
-
-                    </svg>
-
-                </button>
-                //Desktop home button
-                <div>
-                    <Link
-                        href="/"
-                        passHref
-                        legacyBehavior
-                        aira-labal="Back to home"
-                        className="hover:cursor-pointer"
-                    >
-
-                        <Image 
-                            src={jsosifbanner}
-                            alt="Logo"
-                            height={80}
-                            width={300}
-                            className="h-12 md:h-16 xl:h-20 object-contain hover:cursor-pointer"
-                        />
-
-                    </Link>
-                </div>
-                //Navigation Links
-                <nav className="xl:flex space-x-8 text-gray-600 hidden">
-                    {paths.map(({ name, href }) => {
-                        return (
-                            <Link
-                                href={href}
-                                passHref
-                                legacyBehavior
-                                key={name}
-                                aria-label={`Navigate to ${name}`}
-                                className={"hover:cursor-pointer"}
-                            >
-                                <a
-                                    aira-disabled="true"
-                                    className={`hover:text-[#0E5791] hover:cursor-pointer ${
-                                        pathname === href && "text-primary"
-                                    }`}
-                                >
-                                    {name}
-                                </a>
-                            </Link>
-                        );
-                    })}
-                </nav>
-            </div>
-        </header>
-    );
+                  {name}
+                </a>
+              </Link>
+            );
+          })}
+        </nav>
+      </div>
+    </header>
+  );
 }
